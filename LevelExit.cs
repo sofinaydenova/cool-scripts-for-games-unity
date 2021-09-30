40be19d2d1711488a2cfd244cc271bb9df16f818
__________sofi____________(✧ω✧) <53216481+sofinaydenova@users.noreply.github.com>
2021-09-30 14:31:03 +0300

    Level exit --> Next level script
    wrote a level exit script 
    ヽ(o＾▽＾o)ノ

---
+++ LevelExit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float LevelLoadDelay = 2f; // starts in 2 seconds
+    [SerializeField] float LeveLExitSlowMoFactor = 0.2f; // character moves in slowmotion
+    [SerializeField] GameObject winParticles; // some stuff to make the completion of the level more valuable
+    private void Start()
+    {
+      
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(Gate());
+    }
+        
+    IEnumerator Gate()
+    {
+        Time.timeScale = LeveLExitSlowMoFactor;
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+        Time.timeScale = 1f; // normal time scale
+        // GetComponent<AudioSource>().Play(); // if we have an Audio to play
+        winParticles.SetActive(true);
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+        
+    }
+}
